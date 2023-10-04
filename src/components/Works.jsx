f0701672be86e50bf0195156a1c8f9b09e69d0f5
PromiseUdo <info.promiseudo@gmail.com>
2023-10-04 07:30:33 +0100

    update

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,11 @@
 import styles from "../styles/Works.module.scss";
 import Section from "./Section";
+import Portfolio1 from "../assets/portfolio-1.png";
+import Portfolio2 from "../assets/portfolio-2.png";
+import Portfolio3 from "../assets/portfolio-3.png";
+import Portfolio4 from "../assets/portfolio-4.png";
+import Portfolio5 from "../assets/portfolio-5.png";
+import Portfolio6 from "../assets/portfolio-6.png";
 const Works = () => {
   return (
     <Section
@@ -9,7 +15,16 @@ const Works = () => {
       description="I take pride in paying attention to the smallest details and making sure that my work is pixel perfect."
       id="skills"
     >
-      Works
+      <div className={styles.container}>
+        <div className={styles.worksImgs}>
+          <img src={Portfolio1} alt="worksImg" className={styles.worksImg} />
+          <img src={Portfolio2} alt="worksImg" className={styles.worksImg} />
+          <img src={Portfolio3} alt="worksImg" className={styles.worksImg} />
+          <img src={Portfolio4} alt="worksImg" className={styles.worksImg} />
+          <img src={Portfolio5} alt="worksImg" className={styles.worksImg} />
+          <img src={Portfolio6} alt="worksImg" className={styles.worksImg} />
+        </div>
+      </div>
     </Section>
   );
 };

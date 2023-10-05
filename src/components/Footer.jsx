430c41a842fbaca306c4d9a7e2b1bb1d467c5b5d
PromiseUdo <info.promiseudo@gmail.com>
2023-10-05 21:50:49 +0100

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,7 @@ const Footer = () => {
     <footer className={styles.footer}>
       <p>Copyright &#169; 2023 Vera Erumaka. Made with </p>
       <BsFillSuitHeartFill style={{ margin: "0 0.2rem" }} />
+      <p>and CSS </p>
     </footer>
   );
 };

c7a4da01084d152492063fefdf8da70105147eec
PromiseUdo <info.promiseudo@gmail.com>
2023-10-05 19:52:14 +0100

    update

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -4,6 +4,59 @@ import Portfolio1 from "../assets/portfolio-1.png";
 import Portfolio2 from "../assets/portfolio-2.png";
 import Portfolio3 from "../assets/portfolio-3.png";
 import Portfolio4 from "../assets/portfolio-4.png";
+import { AiOutlineLink } from "react-icons/ai";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "LogiKeep Web App",
+    description: "Dashboard Design",
+    image: Portfolio1,
+  },
+  {
+    id: 2,
+    title: "Afrimail Web App",
+    description: "Logistics Mgmt App",
+    image: Portfolio2,
+  },
+  {
+    id: 3,
+    title: "LogiKeep Web App",
+    description: "Auth Screen Design",
+    image: Portfolio3,
+  },
+  {
+    id: 4,
+    title: "Credib Mobile App",
+    description: "Event Planning App",
+    image: Portfolio4,
+  },
+];
+
+const ProjectCard = ({ imgUrl, title, description }) => {
+  return (
+    <div className={styles.cardContainer}>
+      <div
+        style={{
+          background: `url(${imgUrl})`,
+          backgroundSize: "cover",
+          // backgroundRepeat: "no-repeat",
+        }}
+        className={styles.cardInner}
+      >
+        <div className={styles.overlay}>
+          <a href="/" className={styles.linkWrapper}>
+            <AiOutlineLink className={styles.linkIcon} />
+          </a>
+        </div>
+      </div>
+      <div className={styles.cardText}>
+        <h5>{title}</h5>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+};
 
 const Works = () => {
   return (
@@ -16,7 +69,16 @@ const Works = () => {
     >
       <div className={styles.container}>
         <div className={styles.worksImgs}>
-          <div className={styles.portfolioWrap}>
+          {projectsData.map((project, idx) => (
+            <ProjectCard
+              key={idx}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+            />
+          ))}
+
+          {/* <div className={styles.portfolioWrap}>
             <img src={Portfolio1} alt="worksImg" className={styles.worksImg} />
             <div className={styles.portfolioInfo}>
               <h4>LogiKeep Web app</h4>
@@ -43,7 +105,7 @@ const Works = () => {
               <h4>Credib Mobile app</h4>
               <p>Task Screen</p>
             </div>
-          </div>
+          </div> */}
         </div>
         <a
           target="_blank"

9c1f583b29b7c1304f5c6e52f41a6d127306ac85
PromiseUdo <info.promiseudo@gmail.com>
2023-10-06 01:26:54 +0100

    update

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,7 +8,7 @@ const Skills = () => {
   return (
     <Section
       className={styles.skills}
-      title="Learn More What I offer"
+      title="Learn More About What I offer"
       subtitle="What I do"
       description="I am a skilled Product Designer with experience in creating visually appealing and user-friendly website and app designs. These are the services I render."
       id="skills"

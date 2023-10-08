f56cf038ae35053334834a1fc848596e3a8746a7
PromiseUdo <info.promiseudo@gmail.com>
2023-10-08 06:31:00 +0100

    remove stale code

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -23,23 +23,21 @@ const About = () => {
             viewport={{ root: scrollRef }}
             className={styles.skillBar}
           >
-            <div className={styles.skillBarText}>
-              <p>
-                I am currently a product designer at Aceplora and I take
-                quarterly bootcamp trainings. I&apos;ve mentored and trained
-                designers to see beyond the tools and deliver easy and enjoyable
-                products that solves real problems.
-              </p>
-              <p>
-                When I&apos;m not designing, I am either seeing a movie, hitting
-                the gym or simply being awesome. &#128513;
-              </p>
+            <p className={styles.skillBarText}>
+              I am currently a product designer at Aceplora and I take quarterly
+              bootcamp trainings. I&apos;ve mentored and trained designers to
+              see beyond the tools and deliver easy and enjoyable products that
+              solves real problems.
+            </p>
+            <p className={styles.skillBarText}>
+              When I&apos;m not designing, I am either seeing a movie, hitting
+              the gym or simply being awesome. &#128513;
+            </p>
 
-              <p>
-                I am open to both full-time remote and contract roles. Feel free
-                to reach out to me to bring your ideas into life.
-              </p>
-            </div>
+            <p className={styles.skillBarText}>
+              I am open to both full-time remote and contract roles. Feel free
+              to reach out to me to bring your ideas into life.
+            </p>
           </motion.div>
         </div>
 
@@ -57,9 +55,12 @@ const About = () => {
               viewport={{ root: scrollRef }}
               className={styles.process}
             >
-              <div className={styles.skillBarText}>
-                <motion.p whileHover={{ scale: 1.01 }}>Understand</motion.p>
-              </div>
+              <motion.p
+                className={styles.skillBarText}
+                whileHover={{ scale: 1.01 }}
+              >
+                Understand
+              </motion.p>
             </motion.div>
             <motion.div
               initial={{ opacity: 0, y: 10, scale: 0.9 }}
@@ -68,9 +69,12 @@ const About = () => {
               viewport={{ root: scrollRef }}
               className={styles.process}
             >
-              <div className={styles.skillBarText}>
-                <motion.p whileHover={{ scale: 1.01 }}>UX Research</motion.p>
-              </div>
+              <motion.p
+                className={styles.skillBarText}
+                whileHover={{ scale: 1.01 }}
+              >
+                UX Research
+              </motion.p>
             </motion.div>
             <motion.div
               initial={{ opacity: 0, y: 10, scale: 0.9 }}
@@ -79,9 +83,12 @@ const About = () => {
               viewport={{ root: scrollRef }}
               className={styles.process}
             >
-              <div className={styles.skillBarText}>
-                <motion.p whileHover={{ scale: 1.01 }}>Analyze</motion.p>
-              </div>
+              <motion.p
+                className={styles.skillBarText}
+                whileHover={{ scale: 1.01 }}
+              >
+                Analyze
+              </motion.p>
             </motion.div>
             <motion.div
               initial={{ opacity: 0, y: 10, scale: 0.9 }}
@@ -90,9 +97,12 @@ const About = () => {
               viewport={{ root: scrollRef }}
               className={styles.process}
             >
-              <div className={styles.skillBarText}>
-                <motion.p whileHover={{ scale: 1.01 }}>Prototype</motion.p>
-              </div>
+              <motion.p
+                className={styles.skillBarText}
+                whileHover={{ scale: 1.01 }}
+              >
+                Prototype
+              </motion.p>
             </motion.div>
             <motion.div
               initial={{ opacity: 0, y: 10, scale: 0.9 }}
@@ -101,11 +111,12 @@ const About = () => {
               viewport={{ root: scrollRef }}
               className={styles.process}
             >
-              <div className={styles.skillBarText}>
-                <motion.p whileHover={{ scale: 1.01 }}>
-                  Testing/Validation
-                </motion.p>
-              </div>
+              <motion.p
+                className={styles.skillBarText}
+                whileHover={{ scale: 1.01 }}
+              >
+                Testing/Validation
+              </motion.p>
             </motion.div>
             <motion.div
               initial={{ opacity: 0, y: 10, scale: 0.9 }}
@@ -114,9 +125,12 @@ const About = () => {
               viewport={{ root: scrollRef }}
               className={styles.process}
             >
-              <div className={styles.skillBarText}>
-                <motion.p whileHover={{ scale: 1.01 }}>Hand Over</motion.p>
-              </div>
+              <motion.p
+                className={styles.skillBarText}
+                whileHover={{ scale: 1.01 }}
+              >
+                Hand Over
+              </motion.p>
             </motion.div>
           </div>
         </div>

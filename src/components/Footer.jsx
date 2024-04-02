3183558474e771d71531fbef334f09e36af11ac6
PromiseUdo <info.promiseudo@gmail.com>
2024-04-02 10:43:31 +0100

    date

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import styles from "../styles/Footer.module.scss";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>Copyright &#169; 2023 Vera Erumaka </p>
+      <p>Copyright &#169; 2024 Vera Erumaka </p>
     </footer>
   );
 };
